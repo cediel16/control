2e1a7eafe5162ce542ac1919974bf181bbeffaff
Johel Cediel <cediel16@gmail.com>
2013-05-21 20:39:59 -0430

    Actualizacion de la base de datos

--- control.sql
+++ control.sql
@@ -4,10 +4,23 @@
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
-SET standard_conforming_strings = off;
+SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET escape_string_warning = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
 
 SET search_path = public, pg_catalog;
 
@@ -35,8 +48,8 @@ ALTER TABLE public.cargos OWNER TO johel;
 CREATE SEQUENCE cargos_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -47,13 +60,6 @@ ALTER TABLE public.cargos_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE cargos_id_seq OWNED BY cargos.id;
-
-
---
--- Name: cargos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('cargos_id_seq', 3, true);
 
 
 --
@@ -76,8 +82,8 @@ ALTER TABLE public.configuraciones OWNER TO johel;
 CREATE SEQUENCE configuraciones_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -88,13 +94,6 @@ ALTER TABLE public.configuraciones_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE configuraciones_id_seq OWNED BY configuraciones.id;
-
-
---
--- Name: configuraciones_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('configuraciones_id_seq', 1, true);
 
 
 --
@@ -120,8 +119,8 @@ ALTER TABLE public.documentos OWNER TO johel;
 CREATE SEQUENCE documentos_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -132,13 +131,6 @@ ALTER TABLE public.documentos_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE documentos_id_seq OWNED BY documentos.id;
-
-
---
--- Name: documentos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('documentos_id_seq', 43, true);
 
 
 --
@@ -166,8 +158,8 @@ ALTER TABLE public.estaciones OWNER TO johel;
 CREATE SEQUENCE estaciones_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -178,13 +170,6 @@ ALTER TABLE public.estaciones_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE estaciones_id_seq OWNED BY estaciones.id;
-
-
---
--- Name: estaciones_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('estaciones_id_seq', 50, true);
 
 
 --
@@ -200,8 +185,12 @@ CREATE TABLE movimientos (
     descripcion text NOT NULL,
     orden integer NOT NULL,
     horas integer NOT NULL,
-    observacion text,
-    "timestamp" integer
+    ejecutado character varying(5) DEFAULT 'no'::character varying NOT NULL,
+    testigo character varying(5) DEFAULT 'no'::character varying,
+    CONSTRAINT movimientos_ejecutado_check CHECK ((((ejecutado)::text = 'si'::text) OR ((ejecutado)::text = 'no'::text))),
+    CONSTRAINT movimientos_horas_check CHECK ((horas >= 0)),
+    CONSTRAINT movimientos_orden_check CHECK ((orden > 0)),
+    CONSTRAINT movimientos_testigo_check CHECK ((((testigo)::text = 'si'::text) OR ((testigo)::text = 'no'::text)))
 );
 
 
@@ -214,8 +203,8 @@ ALTER TABLE public.movimientos OWNER TO johel;
 CREATE SEQUENCE movimientos_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -226,13 +215,6 @@ ALTER TABLE public.movimientos_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE movimientos_id_seq OWNED BY movimientos.id;
-
-
---
--- Name: movimientos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('movimientos_id_seq', 17, true);
 
 
 --
@@ -256,8 +238,8 @@ ALTER TABLE public.permisos OWNER TO johel;
 CREATE SEQUENCE permisos_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -271,10 +253,38 @@ ALTER SEQUENCE permisos_id_seq OWNED BY permisos.id;
 
 
 --
--- Name: permisos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+-- Name: respuestas; Type: TABLE; Schema: public; Owner: johel; Tablespace: 
 --
 
-SELECT pg_catalog.setval('permisos_id_seq', 18, true);
+CREATE TABLE respuestas (
+    id integer NOT NULL,
+    movimiento_fkey integer NOT NULL,
+    respuesta text NOT NULL,
+    "timestamp" integer NOT NULL
+);
+
+
+ALTER TABLE public.respuestas OWNER TO johel;
+
+--
+-- Name: respuestas_id_seq; Type: SEQUENCE; Schema: public; Owner: johel
+--
+
+CREATE SEQUENCE respuestas_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.respuestas_id_seq OWNER TO johel;
+
+--
+-- Name: respuestas_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: johel
+--
+
+ALTER SEQUENCE respuestas_id_seq OWNED BY respuestas.id;
 
 
 --
@@ -283,7 +293,7 @@ SELECT pg_catalog.setval('permisos_id_seq', 18, true);
 
 CREATE TABLE roles (
     id integer NOT NULL,
-    rol character varying(20) NOT NULL,
+    rol text NOT NULL,
     status character varying(20) DEFAULT 'activo'::character varying NOT NULL
 );
 
@@ -297,8 +307,8 @@ ALTER TABLE public.roles OWNER TO johel;
 CREATE SEQUENCE roles_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -309,13 +319,6 @@ ALTER TABLE public.roles_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE roles_id_seq OWNED BY roles.id;
-
-
---
--- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('roles_id_seq', 14, true);
 
 
 --
@@ -338,8 +341,8 @@ ALTER TABLE public.roles_permisos OWNER TO johel;
 CREATE SEQUENCE roles_permisos_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -353,21 +356,13 @@ ALTER SEQUENCE roles_permisos_id_seq OWNED BY roles_permisos.id;
 
 
 --
--- Name: roles_permisos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('roles_permisos_id_seq', 27, true);
-
-
---
 -- Name: rutas; Type: TABLE; Schema: public; Owner: johel; Tablespace: 
 --
 
 CREATE TABLE rutas (
     id integer NOT NULL,
     ruta character varying(50) NOT NULL,
-    status character varying(20) DEFAULT 'activo'::character varying NOT NULL,
-    bloqueado character varying(2) DEFAULT 'no'::character varying
+    status character varying(20) DEFAULT 'activo'::character varying NOT NULL
 );
 
 
@@ -380,8 +375,8 @@ ALTER TABLE public.rutas OWNER TO johel;
 CREATE SEQUENCE rutas_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -392,13 +387,6 @@ ALTER TABLE public.rutas_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE rutas_id_seq OWNED BY rutas.id;
-
-
---
--- Name: rutas_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('rutas_id_seq', 14, true);
 
 
 --
@@ -421,8 +409,8 @@ ALTER TABLE public.unidades OWNER TO johel;
 CREATE SEQUENCE unidades_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -433,13 +421,6 @@ ALTER TABLE public.unidades_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE unidades_id_seq OWNED BY unidades.id;
-
-
---
--- Name: unidades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('unidades_id_seq', 89, true);
 
 
 --
@@ -467,8 +448,8 @@ ALTER TABLE public.usuarios OWNER TO johel;
 CREATE SEQUENCE usuarios_id_seq
     START WITH 1
     INCREMENT BY 1
-    NO MAXVALUE
     NO MINVALUE
+    NO MAXVALUE
     CACHE 1;
 
 
@@ -479,13 +460,6 @@ ALTER TABLE public.usuarios_id_seq OWNER TO johel;
 --
 
 ALTER SEQUENCE usuarios_id_seq OWNED BY usuarios.id;
-
-
---
--- Name: usuarios_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
---
-
-SELECT pg_catalog.setval('usuarios_id_seq', 11, true);
 
 
 --
@@ -528,6 +502,13 @@ ALTER TABLE ONLY movimientos ALTER COLUMN id SET DEFAULT nextval('movimientos_id
 --
 
 ALTER TABLE ONLY permisos ALTER COLUMN id SET DEFAULT nextval('permisos_id_seq'::regclass);
+
+
+--
+-- Name: id; Type: DEFAULT; Schema: public; Owner: johel
+--
+
+ALTER TABLE ONLY respuestas ALTER COLUMN id SET DEFAULT nextval('respuestas_id_seq'::regclass);
 
 
 --
@@ -577,6 +558,13 @@ COPY cargos (id, cargo, status) FROM stdin;
 
 
 --
+-- Name: cargos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('cargos_id_seq', 3, true);
+
+
+--
 -- Data for Name: configuraciones; Type: TABLE DATA; Schema: public; Owner: johel
 --
 
@@ -586,13 +574,26 @@ COPY configuraciones (id, dias_laborables, horas_laborables_por_dia) FROM stdin;
 
 
 --
+-- Name: configuraciones_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('configuraciones_id_seq', 1, true);
+
+
+--
 -- Data for Name: documentos; Type: TABLE DATA; Schema: public; Owner: johel
 --
 
 COPY documentos (id, titulo, descripcion, status, "timestamp", ruta_fkey) FROM stdin;
-42	Contrato	Firma de contrato para el ciudadano Johel Cediel, por un periodo de 6 meses.	en curso	1368552791	12
-43	Liquidación	dasdas sadgsd gsfh sfhs	en curso	1368555412	14
+50	asdasdas	asd as dasd asda	en curso	1368737563	14
 \.
+
+
+--
+-- Name: documentos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('documentos_id_seq', 50, true);
 
 
 --
@@ -610,25 +611,35 @@ COPY estaciones (id, ruta_fkey, unidad_fkey, cargo_fkey, usuario_fkey, orden, ho
 45	13	87	3	9	4	2	232323
 46	13	86	2	5	5	2	3242342342
 47	13	87	3	10	6	2	212312313
-49	14	85	1	10	1	2	12121313
-48	14	85	1	11	2	1	121212
-50	14	84	1	3	3	60	e4rwgfs hhs ghhgg h
+49	14	85	1	4	1	2	12121313
+48	14	85	1	3	2	1	das dasdasdasdsad asd asd asd as da
+50	14	84	1	4	3	60	e4rwgfs hhs ghhgg h
 \.
+
+
+--
+-- Name: estaciones_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('estaciones_id_seq', 50, true);
 
 
 --
 -- Data for Name: movimientos; Type: TABLE DATA; Schema: public; Owner: johel
 --
 
-COPY movimientos (id, documento_fkey, unidad_fkey, cargo_fkey, usuario_fkey, descripcion, orden, horas, observacion, "timestamp") FROM stdin;
-11	42	85	1	11	asdasdasdasda	1	2	\N	\N
-12	42	85	2	7	safsfsfsdf f sf	2	3	\N	\N
-13	42	88	1	8	Verificación de existencia	3	5	\N	\N
-14	42	85	1	8	xxxxxxxxxxxxxxxxxxxxxxx	4	2	\N	\N
-15	43	85	1	10	12121313	1	2	\N	\N
-16	43	85	1	11	121212	2	1	\N	\N
-17	43	84	1	3	e4rwgfs hhs ghhgg h	233	60	\N	\N
+COPY movimientos (id, documento_fkey, unidad_fkey, cargo_fkey, usuario_fkey, descripcion, orden, horas, ejecutado, testigo) FROM stdin;
+1	50	85	1	4	12121313	1	2	si	no
+2	50	85	1	3	das dasdasdasdsad asd asd asd as da	2	1	si	no
+3	50	84	1	4	e4rwgfs hhs ghhgg h	3	60	si	no
 \.
+
+
+--
+-- Name: movimientos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('movimientos_id_seq', 3, true);
 
 
 --
@@ -654,7 +665,40 @@ COPY permisos (id, permiso, descripcion, status) FROM stdin;
 16	roles.acceso	Permite el acceso al modulo de Roles	activo
 17	documentos.acceso	Permite el acceso a modulo de Documentos	activo
 18	documentos.insertar	Permite añadir nuevos documentos	activo
+19	usuarios.perfil	Permite ver tu perfil de usuario	activo
+20	usuarios.activar	Permite activar el usuario	activo
+21	usuarios.desactivar	Permite desactivar el usuario	activo
+22	usuarios.bloquear	Permite bloquear a un usuario	activo
+23	roles.insertar	Permite añadir un rol	activo
+24	roles.permisos	Permite gestionar los permisos para un rol	activo
+25	roles.activar	Permite activar los roles eliminados	activo
+26	roles.eliminar	Permite eliminar roles	activo
 \.
+
+
+--
+-- Name: permisos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('permisos_id_seq', 26, true);
+
+
+--
+-- Data for Name: respuestas; Type: TABLE DATA; Schema: public; Owner: johel
+--
+
+COPY respuestas (id, movimiento_fkey, respuesta, "timestamp") FROM stdin;
+101	1	asdasdas	1369170181
+102	2	asdasdasd asd asdasdassssdasd	1369171951
+103	3	vkddkjdfkjdf gkjdfkj dfg kdgkad ad gjhadghadkghadk gha ghk  ghkad ghkadghkadh gkghdhg gh gh ghkj ghghkj ghk ghkad ghkjgh kadfhgkadghkadghk adh gkadh gk adhg kajdhg kj adhg ka dh kahd jhadg hadk gjhadgj h akdgfhk adf gh kad gfhkadhgkadg gaieghagjhakdghakdghagha ghadk	1369171968
+\.
+
+
+--
+-- Name: respuestas_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('respuestas_id_seq', 103, true);
 
 
 --
@@ -662,20 +706,27 @@ COPY permisos (id, permiso, descripcion, status) FROM stdin;
 --
 
 COPY roles (id, rol, status) FROM stdin;
+1	Administrador General	activo
+14	asd asd	eliminado
+7	asdasda	eliminado
+9	Cediel	eliminado
+6	Esto es una prueba	eliminado
+5	Prueba	eliminado
+8	qqq	eliminado
+10	qqqq	eliminado
+11	qqqqq	eliminado
+12	qqqqqqqq	eliminado
+13	sdfgg	eliminado
+4	Secretaria	eliminado
 3	Responsable	activo
-5	Prueba	activo
-7	asdasda	activo
-8	qqq	activo
-9	Cediel	activo
-10	qqqq	activo
-11	qqqqq	activo
-12	qqqqqqqq	activo
-13	sdfgg	activo
-6	Esto es una prueba	activo
-4	Secretaria	activo
-14	asd asd	activo
-1	Administrador 2	activo
 \.
+
+
+--
+-- Name: roles_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('roles_id_seq', 14, true);
 
 
 --
@@ -691,28 +742,50 @@ COPY roles_permisos (id, rol_fkey, permiso_fkey) FROM stdin;
 10	1	10
 11	1	11
 12	1	12
-13	1	13
 14	1	14
 20	1	4
 21	1	3
 22	1	2
 23	1	1
 24	1	15
-25	1	16
 26	1	17
 27	1	18
+28	1	19
+29	1	20
+30	1	21
+31	1	13
+32	1	22
+33	1	16
+34	1	23
+35	1	24
+36	1	25
+37	1	26
 \.
+
+
+--
+-- Name: roles_permisos_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('roles_permisos_id_seq', 37, true);
 
 
 --
 -- Data for Name: rutas; Type: TABLE DATA; Schema: public; Owner: johel
 --
 
-COPY rutas (id, ruta, status, bloqueado) FROM stdin;
-12	Control de Obras Públicas	activo	si
-14	Puntos de Cuenta	activo	si
-13	Prueba	activo	si
+COPY rutas (id, ruta, status) FROM stdin;
+12	Control de Obras Públicas	activo
+14	Puntos de Cuenta	activo
+13	Prueba	activo
 \.
+
+
+--
+-- Name: rutas_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('rutas_id_seq', 14, true);
 
 
 --
@@ -731,6 +804,13 @@ COPY unidades (id, unidad, status) FROM stdin;
 
 
 --
+-- Name: unidades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('unidades_id_seq', 89, true);
+
+
+--
 -- Data for Name: usuarios; Type: TABLE DATA; Schema: public; Owner: johel
 --
 
@@ -739,12 +819,20 @@ COPY usuarios (id, usuario, clave, cedula, nombre, status, rol_fkey, email) FROM
 5	jolibert.carolina@hotmail.com	123456	19000650	Jolibert Cediel	activo	1	jolibert.carolina@hotmail.com
 6	jcct19@hotmail.com	123456	15333946	José Cediel	activo	1	jcct19@hotmail.com
 7	heriberto.cediel@hotmail.com	123456	5027147	Heriberto Cediel	activo	1	heriberto.cediel@hotmail.com
-10	admin@localhost2.com	111111	33333333	3333333333	activo	1	admin@localhost2.com
 8	admin@localhost.com	123123123	99999995	Pedro Perez	bloqueado	1	admin@localhost.com
-3	alex@teran.com	123	17258025	Alexander Teran	inactivo	1	alex@teran.com
 9	admin@localhos1.com	123123123	12222222	12eq2eqwe	bloqueado	1	admin@localhos1.com
+3	alex@teran.com	e10adc3949ba59abbe56e057f20f883e	17258025	Alexander Teran	activo	1	alex@teran.com
+12	teranc@alcaldiadeguacara.gob.ve	123456	19000650	Carolina Teran	activo	3	teranc@alcaldiadeguacara.gob.ve
 11	admin@localhost111.com	4297f44b13955235245b2497399d7a93	12312312	12312312312	bloqueado	1	admin@localhost1.com
+10	admin@localhost2.com	111111	33333333	3333333333	activo	1	admin@localhost2.com
 \.
+
+
+--
+-- Name: usuarios_id_seq; Type: SEQUENCE SET; Schema: public; Owner: johel
+--
+
+SELECT pg_catalog.setval('usuarios_id_seq', 12, true);
 
 
 --
@@ -809,6 +897,14 @@ ALTER TABLE ONLY permisos
 
 ALTER TABLE ONLY permisos
     ADD CONSTRAINT permisos_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: respuestas_pkey; Type: CONSTRAINT; Schema: public; Owner: johel; Tablespace: 
+--
+
+ALTER TABLE ONLY respuestas
+    ADD CONSTRAINT respuestas_pkey PRIMARY KEY (id);
 
 
 --
@@ -960,6 +1056,14 @@ ALTER TABLE ONLY movimientos
 
 ALTER TABLE ONLY movimientos
     ADD CONSTRAINT movimientos_usuario_fkey_fkey FOREIGN KEY (usuario_fkey) REFERENCES usuarios(id);
+
+
+--
+-- Name: respuestas_movimiento_fkey_fkey; Type: FK CONSTRAINT; Schema: public; Owner: johel
+--
+
+ALTER TABLE ONLY respuestas
+    ADD CONSTRAINT respuestas_movimiento_fkey_fkey FOREIGN KEY (movimiento_fkey) REFERENCES movimientos(id);
 
 
 --
